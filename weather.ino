a18b81a4b78327315f997d2fe89b40a334fa56ef
Joel-Therrien <Joel_Therrien@uml.edu>
2016-07-13 21:50:38 -0400

    Update weather.ino

--- weather.ino
+++ weather.ino
@@ -1,55 +1,6 @@
 // This #include statement was automatically added by the Particle IDE.
-#include "spark-plotly.h"
-
-/* SFE_BMP180 altitude example sketch
-
-This sketch shows how to use the Bosch BMP180 pressure sensor
-as an altimiter.
-https://www.sparkfun.com/products/11824
-
-Like most pressure sensors, the BMP180 measures absolute pressure.
-Since absolute pressure varies with altitude, you can use the pressure
-to determine your altitude.
-
-Because pressure also varies with weather, you must first take a pressure
-reading at a known baseline altitude. Then you can measure variations
-from that pressure
-
-Hardware connections:
-
-- (GND) to GND
-+ (VDD) to 3.3V
-
-(WARNING: do not connect + to 5V or the sensor will be damaged!)
-
-You will also need to connect the I2C pins (SCL and SDA) to your
-Arduino. The pins are different on different Arduinos:
-
-Any Arduino pins labeled:  SDA  SCL
-Uno, Redboard, Pro:        A4   A5
-Mega2560, Due:             20   21
-Leonardo:                   2    3
-
-Leave the IO (VDDIO) pin unconnected. This pin is for connecting
-the BMP180 to systems with lower logic levels such as 1.8V
-
-Have fun! -Your friends at SparkFun.
-
-The SFE_BMP180 library uses floating-point equations developed by the
-Weather Station Data Logger project: http://wmrx00.sourceforge.net/
-
-Our example code uses the "beerware" license. You can do anything
-you like with this code. No really, anything. If you find it useful,
-buy me a beer someday.
-
-V10 Mike Grusin, SparkFun Electronics 10/24/2013
-V1.1.2 Updates for Arduino 1.6.4 5/2015
-*/
-
-// Your sketch must #include this library, and the Wire library.
-// (Wire is a standard library included with Arduino.):
-
-#include "SFE_BMP180.h"
+#include "spark-plotly.h"   //library with functions to access plotly (modified from what is available at build.particle.io so that it will work)
+#include "SFE_BMP180.h"     //library taken from Sparkfun's code for accessing the BMP
 
 #define NUM_TRACES 2
 
@@ -59,9 +10,9 @@ SFE_BMP180 pressure;
 
 double baseline; // baseline pressure
 
-char *streaming_tokens[NUM_TRACES] = {"bk9v1hl4gu", "xhaie2ymyg"}; //Enter the tokens you generated under Plotly API settings
+char *streaming_tokens[NUM_TRACES] = {"<token #1>", "<token #2>"}; //Enter the tokens you generated under Plotly API settings
 
-plotly graph = plotly("Joel_Therrien", "f9d2ruzaog", streaming_tokens, "weather", NUM_TRACES); //Create a plotly graph with your username, API key, streaming tokens, and graph name
+plotly graph = plotly("<your plotly username>", "<your API key>", streaming_tokens, "<name the plot>", NUM_TRACES); //Create a plotly graph with your username, API key, streaming tokens, and graph name
 
 void setup()
 {
@@ -70,25 +21,17 @@ void setup()
 
   // Initialize the sensor (it is important to get calibration values stored on the device).
 
-  if (pressure.begin())
+    if (pressure.begin())
     Serial.println("BMP180 init success");
-  else
-  {
-    // Oops, something went wrong, this is usually a connection problem,
-    // see the comments at the top of this sketch for the proper connections.
+    else
+    {
+      // Oops, something went wrong, this is usually a connection problem,
+       // see the comments at the top of this sketch for the proper connections.
 
-    Serial.println("BMP180 init fail (disconnected?)\n\n");
-    while(1); // Pause forever.
-  }
-
-  // Get the baseline pressure:
-  
-  baseline = getPressure();
-  
-  Serial.print("baseline pressure: ");
-  Serial.print(baseline);
-  Serial.println(" mb");  
-  graph.fileopt = "extend"; //This will keep adding data to the file instead of overwriting in case your Core ever needs to restart
+       Serial.println("BMP180 init fail (disconnected?)\n\n");
+       while(1); // Pause forever.
+    }
+    graph.fileopt = "extend"; //This will keep adding data to the file instead of overwriting in case your Core ever needs to restart
     graph.convertTimestamp = "true"; //Convert millis to a date/time stamp
     graph.timezone = "America/New_York"; //Choose your timezone
     graph.init(); //Initialize the graph
@@ -97,40 +40,31 @@ void setup()
 
 void loop()
 {
-  float a,P;
+  float T,P;
   
   // Get a new pressure reading:
 
   P = getPressure();
-
-  // Show the relative altitude difference between
-  // the new reading and the baseline reading:
-
-  a = pressure.altitude(P,baseline);
+  T = getTemperature();
   
-  Serial.print("relative altitude: ");
-  if (a >= 0.0) Serial.print(" "); // add a space for positive numbers
-  Serial.print(a,1);
-  Serial.print(" meters, ");
-  if (a >= 0.0) Serial.print(" "); // add a space for positive numbers
-  Serial.print(a*3.28084,0);
-  Serial.println(" feet");
+  Serial.print("Absolute pressure: ");
+  Serial.print(P,1);
+  Serial.print(" mbar, Temperature: ");
+  if (T >= 0.0) Serial.print(" "); // add a space for positive numbers
+  Serial.print(T,0);
+  Serial.println(" DegC");
   
   unsigned long x = millis(); //Variable to store the time in milliseconds that your Core has been alive
+    graph.plot(x, P, streaming_tokens[0]); //Add a timestamped pressure data point to the graph
+    graph.plot(x, T, streaming_tokens[1]); //Add a timestamped temperature data point to the graph
   
-  graph.plot(x, P, streaming_tokens[0]); //Add a timestamped temperature data point to the graph
-  graph.plot(x, a, streaming_tokens[1]); //Add a timestamped humidity data point to the graph
-//    graph.plot(x, lightVal, streaming_tokens[2]); //Add a timestamped light data point to the graph
-//    graph.plot(x, uvVal, streaming_tokens[3]); //Add a timestamped UV data point to the graph
-  
-  delay(5000);
+  delay(5000);  //wait five seconds between measurements
 }
-
 
 double getPressure()
 {
   char status;
-  double T,P,p_temp=0,p0,a;
+  double T,P,p_temp=0;
   int n, count=0, samples=10;
   for(n=0;n<=samples;n++){
   // You must first get a temperature measurement to perform a pressure reading.
@@ -175,8 +109,8 @@ double getPressure()
         status = pressure.getPressure(p_temp,T);
         if (status != 0)
         {
-          P+=p_temp;
-          count++;
+          P+=p_temp;    //add current pressure measurement to running tally. Will be normalized back to average pressure at the end.
+          count++;  //keep track of how many times the pressure was sucessfully measured
         }
         else Serial.println("error retrieving pressure measurement\n");
       }
@@ -186,9 +120,33 @@ double getPressure()
   }
   else Serial.println("error starting temperature measurement\n");
   }
-  P = P/count;
+  P = P/count;  //divide by number of measurements to get average
   return(P);
 }
 
+double getTemperature()
+{
+  char status;
+  double T;
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
 
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
 
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Use '&T' to provide the address of T to the function.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+  }
+  return(T);
+}
